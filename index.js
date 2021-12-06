387585636b083e110532c632d265c48a272bb01e
Rebecca Mollica <rebecca.mollica@live.it>
2021-12-06 17:49:42 +0100

    feature/lista-della-spesa-node

---
+++ index.js
@@ -0,0 +1,21 @@
+const fs = require('fs').promises;
+const arguments = process.argv.slice(2);
+
+async function readFile(file) {
+    const data = await fs.readFile(file)
+    console.log(data.toString())
+}
+
+console.log(readFile('spesa.txt'))
+
+async function writeFile(file, content) {
+    await fs.writeFile(file, content)
+}
+
+const content = `
+    prodotto: ${arguments[0]},
+    quantità: ${arguments[1]}
+`
+
+// fs.readFile(content);
+fs.writeFile('spesa.txt', content, {flag: 'a+'});
